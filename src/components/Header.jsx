e5c270e841af46873787f033148daddf7e499f5f
Yusuf C <yusufcfly@gmail.com>
2022-07-05 18:35:26 +0200

    phase_02

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,42 @@
-import { useState } from "react";
-import AddTaskForm from "./AddTaskForm";
+import AddTaskForm from './AddTaskForm';
+import { useState } from 'react';
 
-const Header = () => {
+const Header = ({ tasks, setTasks }) => {
   const [show, setShow] = useState(false);
   const [btnStyle, setBtnStyle] = useState({
     name: 'SHOW ADD TASK BAR',
     bgColor: 'purple',
   });
 
-
   const handleShow = () => {
     if (show) {
       setBtnStyle({
         name: 'SHOW ADD TASK BAR',
         bgColor: 'purple',
-      })
-    } else ({
-      name: 'CLOSE ADD TASK BAR',
-      bgColor: 'red',
-    })
+      });
+    } else {
+      setBtnStyle({
+        name: 'CLOSE ADD TASK BAR',
+        bgColor: 'red',
+      });
+    }
     setShow(!show);
   };
+  console.log(show);
 
-  console.log(!show);
-
-
-
-
-
-  return <header className="header">
-    <h1>TASK TRACKER</h1>
-    <button onClick={handleShow} className="btn" style={{backgroundColor: btnStyle.bgColor}}>SHOW ADD TASK BAR</button>
-    {/* Shorcircuit yöntemi */}
-    {show && <AddTaskForm />}
-  </header>;
+  return (
+    <header className="header">
+      <h1>TASK TRACKER</h1>
+      <button
+        onClick={handleShow}
+        className="btn"
+        style={{ backgroundColor: btnStyle.bgColor }}
+      >
+        {btnStyle.name}
+      </button>
+      {show && <AddTaskForm tasks={tasks} setTasks={setTasks} />}
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Header from '../components/Header';
 import ShowTasks from '../components/ShowTasks';
+import { useState, useEffect } from 'react';
+// import data from '../helper/starterData';
 
 const Home = () => {
-  return <div>
-    <Header />
-    <ShowTasks />
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem('tasks')) || []
+  );
 
-    </div>;
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  console.log(tasks);
+  return (
+    <div>
+      <Header tasks={tasks} setTasks={setTasks} />
+      <ShowTasks />
+    </div>
+  );
 };
 
 export default Home;
